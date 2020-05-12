6ca9d5e545ff4d70e268ada68d1d844e7ce15c84
Tim <tianzhonglei@gmail.com>
2020-05-12 14:42:08 +0800

    Adding simpler dnn and simplest cnn.

--- dnn/simpler_dnn.py
+++ dnn/simpler_dnn.py
@@ -1,22 +1,27 @@
 import tensorflow as tf
-from keras import models
-from keras import layers
-from keras.datasets import fashion_mnist
+import matplotlib.pyplot as plt
 
-(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
 
-model = models.Sequential([
-    layers.Flatten.input_shape(28, 28),
-    layers.Dense(128, activation=tf.nn.relu),
-    layers.Dense(10, activation=tf.nn.softmax)
-])
+plt.imshow(training_images[0])
+print(training_labels[0])
+print(training_images[0])
 
-model.compile(optimizer=tf.train.AdamOptimizer, loss="sparse_categorical_crossentropy")
+training_images = training_images / 255.0
+test_images = test_images / 255.0
 
-model.fit(train_images, train_labels, epochs = 5)
+model = tf.keras.models.Sequential([tf.keras.layers.Flatten(),
+                                    tf.keras.layers.Dense(128, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)])
 
-test_loss, test_acc = model.evalutate(test_images, test_labels)
+model.compile(optimizer=tf.keras.optimizers.Adam(),
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
 
+model.fit(training_images, training_labels, epochs=5)
+
+test_loss, test_acc = model.evaluate(test_images, test_labels)
 print(test_acc)
 
-#https://colab.research.google.com/github/lmoroney/mlday-tokyo/blob/master/Lab2-Computer-Vision.ipynb
+# https://colab.research.google.com/github/lmoroney/mlday-tokyo/blob/master/Lab2-Computer-Vision.ipynb
