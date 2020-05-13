e7b781fba8ecc56b820b5a452328fae3c3e4828e
Tim <tianzhonglei@gmail.com>
2020-05-13 10:55:07 +0800

    Update simper dnn using Keras.

--- dnn/simpler_dnn.py
+++ dnn/simpler_dnn.py
@@ -35,6 +35,7 @@ print(test_acc)
 
 '''
 # this part is implemented by tensorflow
+# https://colab.research.google.com/github/lmoroney/mlday-tokyo/blob/master/Lab2-Computer-Vision.ipynb
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
@@ -60,5 +61,31 @@ model.fit(training_images, training_labels, epochs=5)
 
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 print(test_acc)
-# https://colab.research.google.com/github/lmoroney/mlday-tokyo/blob/master/Lab2-Computer-Vision.ipynb
+'''
+
+'''
+# simple predict
+import numpy as np
+from keras.layers import Input, Flatten
+from keras.models import Model
+inputs = Input(shape=(3,2,4))
+
+# Define a model consisting only of the Flatten operation
+prediction = Flatten()(inputs)
+model = Model(inputs=inputs, outputs=prediction)
+
+X = np.arange(0,24).reshape(1,3,2,4)
+print(X)
+#[[[[ 0  1  2  3]
+#   [ 4  5  6  7]]
+#
+#  [[ 8  9 10 11]
+#   [12 13 14 15]]
+#
+#  [[16 17 18 19]
+#   [20 21 22 23]]]]
+model.predict(X)
+#array([[  0.,   1.,   2.,   3.,   4.,   5.,   6.,   7.,   8.,   9.,  10.,
+#         11.,  12.,  13.,  14.,  15.,  16.,  17.,  18.,  19.,  20.,  21.,
+#         22.,  23.]], dtype=float32)
 '''
