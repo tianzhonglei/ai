992ba163b22ee60efc675eaecc9129665117cfef
Tim <tianzhonglei@gmail.com>
2020-05-15 19:56:46 +0800

    Update cnn for VGG.

--- cnn/dog_cat_vgg.py
+++ cnn/dog_cat_vgg.py
@@ -1,16 +1,16 @@
-import keras, os
+import keras,os
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPool2D, Flatten
+from keras.layers import Dense, Conv2D, MaxPool2D , Flatten
 from keras.preprocessing.image import ImageDataGenerator
-from keras.optimizers import Adam
-from keras.callbacks import ModelCheckpoint, EarlyStopping
-import matplotlib.pyplot as plt
+
 
 
 trdata = ImageDataGenerator()
-traindata = trdata.flow_from_directory(directory="data", target_size=(224, 224))
+traindata = trdata.flow_from_directory(directory="C:\\Work\\data\\cats_and_dogs_filtered\\train",target_size=(224,224))
 tsdata = ImageDataGenerator()
-testdata = tsdata.flow_from_directory(directory="test", target_size=(224, 224))
+testdata = tsdata.flow_from_directory(directory="C:\\Work\\data\\cats_and_dogs_filtered\\validation", target_size=(224,224))
+
+
 
 model = Sequential()
 model.add(Conv2D(input_shape=(224, 224, 3), filters=64, kernel_size=(3, 3), padding="same", activation="relu"))
@@ -38,21 +38,24 @@ model.add(Dense(units=2, activation="softmax"))
 
 
 
+
+from keras.optimizers import Adam
 opt = Adam(lr=0.001)
 model.compile(optimizer=opt, loss=keras.losses.categorical_crossentropy, metrics=['accuracy'])
+
 
 model.summary()
 
 
 
-checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False,
-                             mode='auto', period=1)
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+checkpoint = ModelCheckpoint("vgg16_1.h5", monitor='val_acc', verbose=1, save_best_only=True, save_weights_only=False, mode='auto', period=1)
 early = EarlyStopping(monitor='val_acc', min_delta=0, patience=20, verbose=1, mode='auto')
-hist = model.fit_generator(steps_per_epoch=100, generator=traindata, validation_data=testdata, validation_steps=10,
-                           epochs=100, callbacks=[checkpoint, early])
+
+hist = model.fit_generator(steps_per_epoch=100,generator=traindata, validation_data= testdata, validation_steps=10,epochs=1,callbacks=[checkpoint,early])
 
 
-
+import matplotlib.pyplot as plt
 plt.plot(hist.history["acc"])
 plt.plot(hist.history['val_acc'])
 plt.plot(hist.history['loss'])
@@ -60,8 +63,9 @@ plt.plot(hist.history['val_loss'])
 plt.title("model accuracy")
 plt.ylabel("Accuracy")
 plt.xlabel("Epoch")
-plt.legend(["Accuracy", "Validation Accuracy", "loss", "Validation Loss"])
+plt.legend(["Accuracy","Validation Accuracy","loss","Validation Loss"])
 plt.show()
 
 
 #https://juejin.im/post/5d8b27396fb9a04dd85909ab
+#https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip
