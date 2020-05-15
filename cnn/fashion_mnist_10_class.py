fd9b2b3132e03e82584174d08695cc816583bd4b
Tim <tianzhonglei@gmail.com>
2020-05-15 21:59:51 +0800

    Update basic fashion mnist 10 class using keras.

---
+++ cnn/fashion_mnist_10_class.py
@@ -0,0 +1,61 @@
+
+from keras.datasets import fashion_mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+
+(training_images, training_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+training_images = training_images.reshape(60000, 28, 28, 1)
+training_images = training_images / 255.0
+test_images = test_images.reshape(10000, 28, 28, 1)
+test_images = test_images / 255.0
+
+model = Sequential()
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=(28, 28, 1)))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Conv2D(64, (3, 3), activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(10, activation='softmax'))
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+model.fit(training_images, training_labels, epochs=5)
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print('Test loss: {}, Test accuracy: {}'.format(test_loss, test_accuracy * 100))
+
+#Test loss: 0.26751336119174957, Test accuracy: 89.95
+#Test loss: 0.2722022842407227, Test accuracy: 90.13
+
+'''  
+# https://codelabs.developers.google.com/codelabs/tensorflow-lab3-convolutions/#0
+
+import tensorflow as tf
+
+print(tf.__version__)
+mnist = tf.keras.datasets.fashion_mnist
+(training_images, training_labels), (test_images, test_labels) = mnist.load_data()
+training_images = training_images.reshape(60000, 28, 28, 1)
+training_images = training_images / 255.0
+test_images = test_images.reshape(10000, 28, 28, 1)
+test_images = test_images / 255.0
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu', input_shape=(28, 28, 1)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(128, activation='relu'),
+    tf.keras.layers.Dense(10, activation='softmax')
+])
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+model.summary()
+model.fit(training_images, training_labels, epochs=5)
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print('Test loss: {}, Test accuracy: {}'.format(test_loss, test_accuracy * 100))
+#Test loss: 0.26172650865316394, Test accuracy: 90.2899980545044
+'''
